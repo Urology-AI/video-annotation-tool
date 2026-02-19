52f4077065a7415baae658fe0becb0ddcac2caf1
Aditya Dixt <adixit@nyu.edu>
2026-02-19 11:01:24 -0500

    fix: update package-lock.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: '**/*.{jsx,js}',
+    }),
+  ],
   base: '/video-annotation-tool/',
   build: {
     outDir: 'dist',
     sourcemap: false,
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
   },
 });
